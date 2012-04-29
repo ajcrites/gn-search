0df19e49a4ffda4d7fb956e36aecb7b0554306b4
Andrew Crites <ajcrites@gmail.com>
2012-04-28 20:16:40 -0400

    Adding initial plugin code, minor README updates

--- README.md
+++ README.md
@@ -13,6 +13,7 @@ of at least three characters long asynchronously and loads them into an element 
 * PHP 5.3.6 (untested on other versions, but should work all 5.2 to 5.4 versions)
  * PHP `curl` **OR** `fopen` wrappers enabled
 * Wordpress 3.3.2 (untested on other versions)
+* jQuery 1.7.2
 
 ### Instructions
 
@@ -32,4 +33,9 @@ You can use the bare shortcode `[google-news]` to use default options.  Otherwis
 the following options as attributes of the shortcode:
 
 * `chars` -- required number of characters to initiate search (default: 3)
-* `placeholder` -- placeholder text (only in browsers that support the `placeholder` attribute (deafult: none)
+* `placeholder` -- placeholder text (only in browsers that support the `placeholder` attribute (default: none)
+* `id` -- ID of the element to load results into (default: gn-search-container)
+ * **WARNING:** If provided, the element that ordinarily has the results is omitted from the response.  You must
+   provide your own
+
+**NOTE:** The input has an `id` attribute of `gn-search`.
---
+++ src/gn-search.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * The purpose of this file is to create a plugin for a short code for a Google News search
+ * @author Andrew Crites <explosion-pills@aysites.com>
+ * @package gn-search
+ */
+
+class GnSearchShortcode {
+
+   private $errors = new WP_Error;
+
+   public function gnsearch_shortcode_func($atts) {
+      //I hate to use `extract`, but this seems to be the standard
+      extract( shortcode_atts( array(
+         'chars' => 3,
+         'placeholder' => '',
+         'id' => 'gn-search-container',
+      ), $atts ) );
+
+      if (!is_int($chars) || $chars < 1) {
+         $this->errors->add('invalid_chars', __(__METHOD__ . ': you must specify at least 1 character to respond to'));
+      }
+
+      $html = <<<HTML
+         <input type="text" placeholder="$placeholder" id="gn-search" data-chars="$chars" data-container="$id" />
+HTML;
+      if ($id == 'gn-search-container') {
+         $html .= <<<HTML
+            <section id="gn-search-container"></section>
+HTML;
+      }
+      else if (!$id) {
+         $this->errors->add('invalid_id', __(__METHOD__ . ': you must specify an id attribute for the container element'
+            . ' or use the default'));
+         //The input is useless without an id to refer to, so leave it blank
+         return '';
+      }
+      return $html;
+   }
+
+   public function get_errors() {
+      return $this->errors;
+   }
+}
+
+add_shortcode('gn-search', array('GnSearchShortcode', 'gnsearch_shortcode_func'));
+wp_enqueue_scripot('my-ajax-request', plugin_dir_url(__FILE__) . 'gn-search.js', array('jquery'));
+wp_localize_script('my-ajax-request', 'GnSearch', array('ajaxurl' => admin_url('gn-search-ajax.php')));
+?>
